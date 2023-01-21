2850b0834f0abb8cf36e9e062f0bd7bdd9463706
MiaoMint <1981324730@qq.com>
2023-01-22 02:59:14 +0800

    refactor: support miru 1.1.0

--- repo/kk151.js
+++ repo/kk151.js
@@ -1,109 +1,99 @@
-// ==MiruUserScript==
+// ==MiruExpand==
 // @name         动漫之家(kk151)
-// @version      v0.0.2
+// @version      v0.0.3
 // @author       MiaoMint
 // @lang         zh-cn
 // @license      MIT
 // @package      dev.0n0.miru.kk151
 // @icon         https://www.kk151.com/templets/dm/images/favicon.ico
-// ==/MiruUserScript==
+// ==/MiruExpand==
 
-
-const miru = new Miru("https://www.kk151.com");
-
-function getCover(url) {
-    if (url.indexOf("http") == -1) {
-        return `https://www.kk151.com${url}`
+export default class KK151 extends Expand {
+    constructor(name) {
+        super(name, "https://www.kk151.com")
     }
-    return url
-}
-
-// 搜索
-miru.search = async (kw, page) => {
-    const bangumi = []
-    const res = await miru.request(`/search.php?page=${page}&searchword=${kw}`);
-    let pattern = /<div class="video-list1 clearfix">([\s\S]+?)<div class="page clearfix">/g;
-    const bangumisStr = pattern.exec(res)[0]
-    pattern = /<div class="col-md-12 col-sm-6 col-xs-12 p0">([\s\S]+?)<div class="subtitle">/g
-    const m = bangumisStr.match(pattern)
-    m.forEach(e => {
-        const cover = /background: url\((.+?)\);/
-        const title = /title="(.+?)"/
-        const url = /href="(.+?)"/
-        bangumi.push({
-            title: e.match(title)[1],
-            cover: getCover(e.match(cover)[1]),
-            url: e.match(url)[1],
-        })
-    })
-    return bangumi
-};
-
-// 详情
-miru.info = async (url) => {
-    const res = await miru.request(url);
-    const desc = res.match(/id="plot">([\s\S]+?)<\/div>/)[1].replace(/<[^>]+>/ig, "")
-    const title = res.match(/<h4 class="media-heading">(.+?)<\/h4>/)[1]
-    const cover = getCover(res.match(/background: url\((.+?)\);/)[1])
-    const watchUrlGroupsStr = res.match(/<dd([\s\S]+?)<\/dd>/g)
-    const watchurl = new Map()
-    let i = 0
-    if (watchUrlGroupsStr) {
-        watchUrlGroupsStr.forEach(e => {
-            let group = []
-            let lis = e.match(/<li>([\s\S]+?)<\/li>/g)
-            if (!lis) {
-                return
-            }
-            lis.forEach(e => {
-                const name = e.match(/">(.+?)<\/a>/)[1]
-                const url = e.match(/href='(.+?)'/)[1]
-                group.push({
-                    name,
-                    url
-                })
+    getCover(url) {
+        if (url.indexOf("http") == -1) {
+            return `https://www.kk151.com${url}`
+        }
+        return url
+    }
+    async search(kw, page) {
+        const bangumi = []
+        const res = await this.request(`/search.php?page=${page}&searchword=${kw}`);
+        let pattern = /<div class="video-list1 clearfix">([\s\S]+?)<div class="page clearfix">/g;
+        const bangumisStr = pattern.exec(res)[0]
+        pattern = /<div class="col-md-12 col-sm-6 col-xs-12 p0">([\s\S]+?)<div class="subtitle">/g
+        const m = bangumisStr.match(pattern)
+        m.forEach(e => {
+            const cover = /background: url\((.+?)\);/
+            const title = /title="(.+?)"/
+            const url = /href="(.+?)"/
+            bangumi.push({
+                title: e.match(title)[1],
+                cover: this.getCover(e.match(cover)[1]),
+                url: e.match(url)[1],
             })
-            watchurl.set(`线路${++i}`, group)
         })
+        return bangumi
     }
-    return {
-        watchurl,
-        desc,
-        cover,
-        title
-    }
-};
-
-// 最近更新
-miru.new = async () => {
-    const bangumi = []
-    const res = await miru.request(`/search.php?searchtype=5&tid=0`);
-    let pattern = /<div class="video-list1">([\s\S]+?)<div class="page clearfix">/g;
-    const bangumisStr = pattern.exec(res)[0]
-    pattern = /<div class="col-md-4 col-sm-6 col-xs-12 p0">([\s\S]+?)<div class="subtitle">/g
-    const m = bangumisStr.match(pattern)
-    m.forEach(e => {
-        const cover = /background: url\((.+?)\);/
-        const title = /title="(.+?)"/
-        const url = /href="(.+?)"/
-        bangumi.push({
-            title: e.match(title)[1],
-            cover: getCover(e.match(cover)[1]),
-            url: e.match(url)[1],
+    async latest() {
+        const bangumi = []
+        const res = await this.request(`/search.php?searchtype=5&tid=0`);
+        let pattern = /<div class="video-list1">([\s\S]+?)<div class="page clearfix">/g;
+        const bangumisStr = pattern.exec(res)[0]
+        pattern = /<div class="col-md-4 col-sm-6 col-xs-12 p0">([\s\S]+?)<div class="subtitle">/g
+        const m = bangumisStr.match(pattern)
+        m.forEach(e => {
+            const cover = /background: url\((.+?)\);/
+            const title = /title="(.+?)"/
+            const url = /href="(.+?)"/
+            bangumi.push({
+                title: e.match(title)[1],
+                cover: this.getCover(e.match(cover)[1]),
+                url: e.match(url)[1],
+            })
         })
-    })
-    return bangumi
-}
-
-// 观看
-miru.watch = async (url) => {
-    const res = await miru.request(url)
-    return {
-        type: "player",
-        src: res.match(/now="(.+?)"/)[1]
+        return bangumi
+    }
+    async info(url) {
+        const res = await this.request(url);
+        const desc = res.match(/id="plot">([\s\S]+?)<\/div>/)[1].replace(/<[^>]+>/ig, "")
+        const title = res.match(/<h4 class="media-heading">(.+?)<\/h4>/)[1]
+        const cover = this.getCover(res.match(/background: url\((.+?)\);/)[1])
+        const watchUrlGroupsStr = res.match(/<dd([\s\S]+?)<\/dd>/g)
+        const watchurl = new Map()
+        let i = 0
+        if (watchUrlGroupsStr) {
+            watchUrlGroupsStr.forEach(e => {
+                let group = []
+                let lis = e.match(/<li>([\s\S]+?)<\/li>/g)
+                if (!lis) {
+                    return
+                }
+                lis.forEach(e => {
+                    const name = e.match(/">(.+?)<\/a>/)[1]
+                    const url = e.match(/href='(.+?)'/)[1]
+                    group.push({
+                        name,
+                        url
+                    })
+                })
+                watchurl.set(`线路${++i}`, group)
+            })
+        }
+        return {
+            watchurl,
+            desc,
+            cover,
+            title
+        }
+    }
+    async watch(url) {
+        const res = await this.request(url)
+        return {
+            type: "player",
+            src: res.match(/now="(.+?)"/)[1]
+        }
     }
 }
-
-return miru
-
-
--- repo/sakura2.js
+++ repo/sakura2.js
@@ -1,104 +1,100 @@
-// ==MiruUserScript==
+// ==MiruExpand==
 // @name         Sakura(樱花动漫)2
-// @version      v0.0.2
+// @version      v0.0.3
 // @author       MiaoMint
 // @lang         zh-cn
 // @license      MIT
 // @icon         http://www.yinghuacd.com/js/20180601/0601.png
 // @package      dev.0n0.miru.sakura2
-// ==/MiruUserScript==
+// ==/MiruExpand==
 
-
-const miru = new Miru("http://www.yinghuacd.com");
-
-const getCover = (url) => {
-    return `https://wsrv.nl/?url=${url}&w=200&h=300`
-}
-
-// 搜索
-miru.search = async (kw, page) => {
-    const bangumi = []
-    const res = await miru.request(`/search/${kw}/?page=${page - 1}`);
-    let pattern = /class="lpic">([\s\S]+?)<div class="pages">/g;
-    const bangumisStr = pattern.exec(res)[0]
-    pattern = /<li>([\s\S]+?)<\/li>/g
-    bangumisStr.match(pattern).forEach(e => {
-        console.log(e);
-        const cover = /src="(.+?)"/
-        const title = /alt="(.+?)"/
-        const url = /href="(.+?)"/
-        console.log(e.match(title));
-        bangumi.push({
-            cover: getCover(e.match(cover)[1]),
-            title: e.match(title)[1],
-            url: e.match(url)[1],
-        })
-    })
-    return bangumi
-};
-
-// 详情
-miru.info = async (url) => {
-    const res = await miru.request(`${url}`);
-    const desc = res.match(/<div class="info">([\s\S]+?)<\/div>/)[1]
-    const title = res.match(/<h1>(.+?)<\/h1>/)[1]
-    const cover = res.match(/src="(.+?)" alt=/)[1]
-    const watchUrlGroupsStr = res.match(/class="movurl"([\s\S]+?)>([\s\S]+?)<\/div>/g)
-    const watchurl = new Map()
-    let i = 0
-    watchUrlGroupsStr.forEach(e => {
-        let group = []
-        let lis = e.match(/<li>([\s\S]+?)<\/li>/g)
-        if (!lis) {
-            return
-        }
-        lis.forEach(e => {
-            const name = e.match(/">(.+?)<\/a>/)[1]
-            const url = e.match(/href="(.+?)"/)[1]
-            group.push({
-                name,
-                url
+export default class Sakura2 extends Expand {
+    constructor(name) {
+        super(name, "http://www.yinghuacd.com")
+    }
+    getCover(url) {
+        return `https://wsrv.nl/?url=${url}&w=200&h=300`
+    }
+    async search(kw, page) {
+        const bangumi = []
+        const res = await this.request(`/search/${kw}/?page=${page - 1}`);
+        let pattern = /class="lpic">([\s\S]+?)<div class="pages">/g;
+        const bangumisStr = pattern.exec(res)[0]
+        pattern = /<li>([\s\S]+?)<\/li>/g
+        bangumisStr.match(pattern).forEach(e => {
+            console.log(e);
+            const cover = /src="(.+?)"/
+            const title = /alt="(.+?)"/
+            const url = /href="(.+?)"/
+            console.log(e.match(title));
+            bangumi.push({
+                cover: this.getCover(e.match(cover)[1]),
+                title: e.match(title)[1],
+                url: e.match(url)[1],
             })
         })
-        watchurl.set(`线路${++i}`, group)
-    })
-    return {
-        watchurl,
-        desc,
-        cover:getCover(cover),
-        title
-    }
-};
+        return bangumi
+    };
 
-miru.new = async () => {
-    const bangumi = []
-    const res = await miru.request("")
-    let pattern = /class="firs l">([\s\S]+?)class="side r"/g;
-    const bangumisStr = pattern.exec(res)[0]
-    pattern = /<li>([\s\S]+?)<\/li>/g
-    bangumisStr.match(pattern).forEach(e => {
-        console.log(e);
-        const cover = /src="(.+?)"/
-        const title = /alt="(.+?)"/
-        const url = /href="(.+?)"/
-        console.log(e.match(title));
-        bangumi.push({
-            cover: getCover(e.match(cover)[1]),
-            title: e.match(title)[1],
-            url: e.match(url)[1],
+    async info(url) {
+        const res = await this.request(`${url}`);
+        const desc = res.match(/<div class="info">([\s\S]+?)<\/div>/)[1]
+        const title = res.match(/<h1>(.+?)<\/h1>/)[1]
+        const cover = res.match(/src="(.+?)" alt=/)[1]
+        const watchUrlGroupsStr = res.match(/class="movurl"([\s\S]+?)>([\s\S]+?)<\/div>/g)
+        const watchurl = new Map()
+        let i = 0
+        watchUrlGroupsStr.forEach(e => {
+            let group = []
+            let lis = e.match(/<li>([\s\S]+?)<\/li>/g)
+            if (!lis) {
+                return
+            }
+            lis.forEach(e => {
+                const name = e.match(/">(.+?)<\/a>/)[1]
+                const url = e.match(/href="(.+?)"/)[1]
+                group.push({
+                    name,
+                    url
+                })
+            })
+            watchurl.set(`线路${++i}`, group)
         })
-    })
-    return bangumi
-}
+        return {
+            watchurl,
+            desc,
+            cover: this.getCover(cover),
+            title
+        }
+    }
 
-// 观看
-miru.watch = async (url) => {
-    const res = await miru.request(`${url}`)
-    return {
-        type: "iframe",
-        src: `https://tup.yinghuacd.com/?vid=${res.match(/data-vid="(.+?)"/)[1]}`
+    async latest() {
+        const bangumi = []
+        const res = await this.request("")
+        let pattern = /class="firs l">([\s\S]+?)class="side r"/g;
+        const bangumisStr = pattern.exec(res)[0]
+        pattern = /<li>([\s\S]+?)<\/li>/g
+        bangumisStr.match(pattern).forEach(e => {
+            console.log(e);
+            const cover = /src="(.+?)"/
+            const title = /alt="(.+?)"/
+            const url = /href="(.+?)"/
+            console.log(e.match(title));
+            bangumi.push({
+                cover: this.getCover(e.match(cover)[1]),
+                title: e.match(title)[1],
+                url: e.match(url)[1],
+            })
+        })
+        return bangumi
+    }
+    async watch(url) {
+        const res = await this.request(url)
+        return {
+            type: "iframe",
+            src: `https://tup.yinghuacd.com/?vid=${res.match(/data-vid="(.+?)"/)[1]}`
+        }
     }
 }
 
-return miru;
 

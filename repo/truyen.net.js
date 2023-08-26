682fe664c2dd2fa0fd1b8ab299f2e155343507e0
MiaoMint <1981324730@qq.com>
2023-08-26 10:46:12 +0800

    Fix watch section

--- repo/truyen.net.js
+++ repo/truyen.net.js
@@ -11,64 +11,65 @@
 // ==/MiruExtension==
 
 export default class extends Extension {
- async req(url) {
-  return this.request(url, {
-   headers: {
-    "Miru-Url": await this.getSetting("truyen"),
-   },
-  });
- }
+  async req(url) {
+    return this.request(url, {
+      headers: {
+        "Miru-Url": await this.getSetting("truyen"),
+      },
+    });
+  }
 
- async load() {
-  this.registerSetting({
-   title: "Truyen API",
-   key: "truyen",
-   type: "input",
-   description: "Truyen Api Url",
-   defaultValue: "https://comics-api.vercel.app",
-  });
- }
+  async load() {
+    this.registerSetting({
+      title: "Truyen API",
+      key: "truyen",
+      type: "input",
+      description: "Truyen Api Url",
+      defaultValue: "https://comics-api.vercel.app",
+    });
+  }
 
- async latest(page) {
-  const res = await this.req(`/trending-comics?page=${page}`);
-  return res.comics.map((item) => ({
-   title: item.title,
-   url: item.id.toString(),
-   cover: item.thumbnail,
-  }));
- }
-
- async detail(url) {
-  const res = await this.req(`/comics/${url}`);
-  return {
-   title: res.title,
-   cover: res.thumbnail,
-   desc: res.description,
-   episodes: [
-    {
-     title: "Chapters",
-     urls: res.chapters.map((item) => ({
-      name: `Episode ${item.name}`,
+  async latest(page) {
+    const res = await this.req(`/trending-comics?page=${page}`);
+    return res.comics.map((item) => ({
+      title: item.title,
       url: item.id.toString(),
-     })),
-    },
-   ],
-  };
- }
+      cover: item.thumbnail,
+    }));
+  }
 
- async search(kw, page) {
-  const res = await this.req(`/search?q=${kw}&page=${page}`);
-  return res.comics.map((item) => ({
-   title: item.title,
-   url: item.id.toString(),
-   cover: item.thumbnail,
-  }));
- }
+  async detail(url) {
+    const res = await this.req(`/comics/${url}`);
+    return {
+      title: res.title,
+      cover: res.thumbnail,
+      desc: res.description,
+      episodes: [
+        {
+          title: "Chapters",
+          urls: res.chapters.map((item) => ({
+            name: `Episode ${item.name}`,
+            url: `${url}|${item.id}`,
+          })),
+        },
+      ],
+    };
+  }
 
- async watch(url) {
-  const res = await this.request(`/comics/${url}/chapters/${url}`);
-  return {
-   urls: res.images.map((item) => item.src),
-  };
- }
+  async search(kw, page) {
+    const res = await this.req(`/search?q=${kw}&page=${page}`);
+    return res.comics.map((item) => ({
+      title: item.title,
+      url: item.id.toString(),
+      cover: item.thumbnail,
+    }));
+  }
+
+  async watch(url) {
+    const [id, chapter] = url.split("|");
+    const res = await this.request(`/comics/${id}/chapters/${chapter}`);
+    return {
+      urls: res.images.map((item) => item.src),
+    };
+  }
 }

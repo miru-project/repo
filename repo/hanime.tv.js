5f954239acaabb14dc4d2cc0db79a0f6a2092198
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-09-29 16:05:58 +0530

    Finally 😭
    Some buffering issue with 720p else everything good

--- repo/hanime.tv.js
+++ repo/hanime.tv.js
@@ -12,14 +12,49 @@
 // ==/MiruExtension==
 
 export default class extends Extension {
-  async search() {
-    // ToDo
+  async search(kw) {
+    const requestBody = {
+      search_text: kw,
+      tags: [],
+      tags_mode: "AND",
+      brands: [],
+      blacklist: [],
+      order_by: "created_at_unix",
+      ordering: "desc",
+      page: 0,
+    };
+
+    const res = await this.request("", {
+      headers: {
+        "Miru-Url": "https://search.htv-services.com/",
+      },
+      data: requestBody,
+      method: "post",
+    });
+
+    const mangaList = [];
+
+    const hitsArray = JSON.parse(res.hits);
+
+    for (const comic of hitsArray) {
+      const id = comic.id;
+      const title = comic.name;
+      if (!title || !comic.cover_url) continue;
+
+      mangaList.push({
+        title: title,
+        url: id.toString(),
+        cover: comic.cover_url,
+      });
+    }
+
+    return mangaList;
   }
 
   async latest(page) {
     const res = await this.request(`/hanime/recent?page=${page}`);
     return res.reposone.map((item) => ({
-      title: item.titles[0],
+      title: item.name,
       url: item.id.toString(),
       cover: item.cover_url,
     }));
@@ -27,29 +62,30 @@ export default class extends Extension {
 
   async detail(url) {
     const res = await this.request(`/hanime/details?id=${url}`);
+    const epRes = await this.request(`/hanime/link?id=${url}`);
+
     return {
       title: res.name,
       cover: res.poster,
       desc: res.description,
       episodes: [
         {
-          title: "Directory",
-          urls: [
-            {
-              name: `${res.name}`,
-              url: `${res.id}`,
-            },
-          ],
+          title: "Episodes",
+          urls: epRes.data
+            .filter((item) => item.is_guest_allowed !== false)
+            .map((item) => ({
+              name: item.filename != null ? item.filename : `Quality ${item.height}`,
+              url: item.url != null ? item.url : "",
+            })),
         },
       ],
     };
   }
 
   async watch(url) {
-    const res = await this.request(`/hanime/link?id=${url}`);
     return {
       type: "hls",
-      url: res.data[1].url,
+      url: url,
     };
   }
 }

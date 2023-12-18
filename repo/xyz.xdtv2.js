1df70af5c56ebde5fb06bcde3aeb731af25dd59b
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-12-18 11:31:40 +0530

    Fix 好看的2号
    Had to change whole structure 😕

--- repo/xyz.xdtv2.js
+++ repo/xyz.xdtv2.js
@@ -1,109 +1,90 @@
 // ==MiruExtension==
 // @name         好看的2号
-// @version      v0.0.1
+// @version      v0.0.2
 // @author       zj
 // @lang         zh-cn
 // @license      MIT
-// @icon         https://xdtv2.xyz/template/xdtv/static/favicon.ico
+// @icon         https://caocao15.xyz/upload/site/20230913-1/bf5300cb13794e430bff80aa9d4701bb.png
 // @package      xyz.xdtv2
 // @type         bangumi
-// @webSite      https://xdtv2.xyz/
+// @webSite      https://caocao15.xyz
 // @nsfw         true
 // ==/MiruExtension==
 
 export default class extends Extension {
-    constructor() {
-        super("https://xdtv2.xyz/")
+  async search(kw) {
+    const res = await this.request(`/index.php/vod/search.html?wd=${kw}`);
+    const bsxList = await this.querySelectorAll(res, "ul.thumbnail-group.clearfix > li");
+    const bangumi = [];
+    for (const element of bsxList) {
+      const html = await element.content;
+      const url = await this.getAttributeText(html, "a.thumbnail", "href");
+      const title = await this.querySelector(html, "h5 > a").text;
+      const cover = await this.querySelector(html, "img").getAttributeText("src");
+      bangumi.push({
+        title: title.trim(),
+        url: `${url};${cover};${title}`,
+        cover,
+      });
     }
+    return bangumi;
+  }
 
-    async search(kw, page) {
-        const res = await this.request(`/index.php/vod/search/page/${page}/wd/${kw}.html`)
-
-        const ul = /class="content-list "([\s\S]+?)\/ul/g.exec(res)[0]
-        const li = ul.match(/<li class="content-item content-item-2">([\s\S]+?)<\/li>/g)
-        const bangumi = []
-        li.forEach(e => {
-            const title = e.match(/title="(.+?)"/)[1]
-            const url = e.match(/href="(.+?)"/)[1]
-            const cover = e.match(/src="(.+?)"/)[1]
-            let update = ""
-            try{
-                update = e.match(/text-bg-r">(.+?)<\/span>/)[1]
-            }catch (error) {
-                update = ""
-            }
-            bangumi.push({
-                title,
-                url,
-                cover,
-                update
-            })
-        })
-
-        return bangumi
+  async latest() {
+    const res = await this.request("/index.php/vod/type.html");
+    const bsxList = await this.querySelectorAll(res, "ul.thumbnail-group.clearfix > li");
+    const bangumi = [];
+    for (const element of bsxList) {
+      const html = await element.content;
+      const url = await this.getAttributeText(html, "a.thumbnail", "href");
+      const title = await this.querySelector(html, "h5 > a").text;
+      const cover = await this.querySelector(html, "img").getAttributeText("src");
+      bangumi.push({
+        title: title.trim(),
+        url: `${url};${cover};${title}`,
+        cover,
+      });
     }
-    async latest() {
-        const res = await this.request("/index.php/vod/type/id/5.html")
+    return bangumi;
+  }
 
-        const ul = /class="content-list "([\s\S]+?)\/ul/g.exec(res)[0]
-        const li = ul.match(/<li class="content-item content-item-2">([\s\S]+?)<\/li>/g)
-        const bangumi = []
-        li.forEach(e => {
-            const title = e.match(/title="(.+?)"/)[1]
-            const url = e.match(/href="(.+?)"/)[1]
-            const cover = e.match(/src="(.+?)"/)[1]
-            let update = ""
-            try{
-                update = e.match(/text-bg-r">(.+?)<\/span>/)[1]
-            }catch (error) {
-                update = ""
-            }
-            bangumi.push({
-                title,
-                url,
-                cover,
-                update
-            })
-        })
+  async detail(url) {
+    const res = await this.request(url.split(';')[0]);
+    const title = url.split(';')[2];
+    const cover = url.split(';')[1];
+    const desc = title;
+   
+    const episodes = [
+      {
+        title: "国内地址",
+        urls: [
+          {
+            name: title,
+            url: 'https://caocao15.xyz' + url.split(';')[0],
+          },
+        ],
+      },
+    ];
 
-        return bangumi
-    }
+    return {
+      episodes,
+      desc: desc.trim(),
+      cover,
+      title: title.trim(),
+    };
+  }
 
-    async detail(url) {
-        const res = await this.request(url)
-
-        const cover = res.match(/class="img-responsive" src="(.+?)"/)[1]
-        const title = res.match(/name="description" content="(.+?)"/)[1]
-        const desc = title;
-        const originalUrl = url;
-        const dynamicValue = originalUrl.match(/\/detail\/id\/(\d+)\.html/)[1];
-        const modifiedUrl = originalUrl.replace(/\/detail\/id\/(\d+)\.html/, `/play/id/${dynamicValue}/sid/1/nid/1.html`);
-        const episodes = [
-            {
-                'title': '国内地址',
-                'urls': [
-                    {
-                        'name': '小哥,进来看看妹妹吧',
-                        'url': modifiedUrl
-                    }
-                ]
-            }];
-        
-        return {
-            episodes,
-            desc,
-            cover,
-            title
-        }
-    }
-
-    async watch(url) {
-        const res = await this.request(url)
-        const m3u8 = res.match(/,"url":"(.+?)","url_next"/)[1].replace(/\\\//g, '/');
+  async watch(url) {
+    const res = await this.request("", {
+      headers: {
+        "Miru-Url": url,
+      },
+    });
+    const json = res.match(/<script type="text\/javascript">var player_aaaa=(.+?)<\/script>/);
+    
         return {
             type: "hls",
-            url: m3u8
-        }
-    }
-
+            url: JSON.parse(json[1]).url,
+        };
+     }
 }

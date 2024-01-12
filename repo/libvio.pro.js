6fad9bc66886ff6aa5befd724f5f1d5b00789436
appdevelpo <56633229+appdevelpo@users.noreply.github.com>
2024-01-13 03:05:48 +0800

    fix(libvio):TypeError (#163)

--- repo/libvio.pro.js
+++ repo/libvio.pro.js
@@ -1,6 +1,6 @@
 // ==MiruExtension==
 // @name         libvio
-// @version      v0.0.1
+// @version      v0.0.2
 // @author       appdevelpo
 // @lang         zh
 // @license      MIT
@@ -126,7 +126,18 @@ export default class extends Extension {
   async watch(url) {
     const res = await this.request(url);
     const player_json = res.match(/player_aaaa=(\{.+?\})/)[1];
+    const player_script =  res.match(/src="([\w\/]+?player\.js.+?)"/)[1];
+    console.log(player_script)
+    const playerRes = await this.request(player_script);
+    // console.log(playerRes)
+    const script_version = playerRes.match(/(\.js\?.+?)"/)[1];
+    // console.log(script_version)
+
     const player_data = JSON.parse(player_json);
+    const embed_script = `/static/player/${player_data.from}${script_version}`;
+    const embedScriptRes = await this.request(embed_script);
+    console.log(embedScriptRes)
+    const embed_url_based = embedScriptRes.match(/src="(.+?)'/)[1]
     var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"; var base64DecodeChars = new Array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1); function base64encode(str) { var out, i, len; var c1, c2, c3; len = str.length; i = 0; out = ""; while (i < len) { c1 = str.charCodeAt(i++) & 0xff; if (i == len) { out += base64EncodeChars.charAt(c1 >> 2); out += base64EncodeChars.charAt((c1 & 0x3) << 4); out += "=="; break } c2 = str.charCodeAt(i++); if (i == len) { out += base64EncodeChars.charAt(c1 >> 2); out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4)); out += base64EncodeChars.charAt((c2 & 0xF) << 2); out += "="; break } c3 = str.charCodeAt(i++); out += base64EncodeChars.charAt(c1 >> 2); out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4)); out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6)); out += base64EncodeChars.charAt(c3 & 0x3F) } return out } function base64decode(str) { var c1, c2, c3, c4; var i, len, out; len = str.length; i = 0; out = ""; while (i < len) { do { c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff] } while (i < len && c1 == -1); if (c1 == -1) break; do { c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff] } while (i < len && c2 == -1); if (c2 == -1) break; out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4)); do { c3 = str.charCodeAt(i++) & 0xff; if (c3 == 61) return out; c3 = base64DecodeChars[c3] } while (i < len && c3 == -1); if (c3 == -1) break; out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2)); do { c4 = str.charCodeAt(i++) & 0xff; if (c4 == 61) return out; c4 = base64DecodeChars[c4] } while (i < len && c4 == -1); if (c4 == -1) break; out += String.fromCharCode(((c3 & 0x03) << 6) | c4) } return out } function utf16to8(str) { var out, i, len, c; out = ""; len = str.length; for (i = 0; i < len; i++) { c = str.charCodeAt(i); if ((c >= 0x0001) && (c <= 0x007F)) { out += str.charAt(i) } else if (c > 0x07FF) { out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F)); out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F)); out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F)) } else { out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F)); out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F)) } } return out } function utf8to16(str) { var out, i, len, c; var char2, char3; out = ""; len = str.length; i = 0; while (i < len) { c = str.charCodeAt(i++); switch (c >> 4) { case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7: out += str.charAt(i - 1); break; case 12: case 13: char2 = str.charCodeAt(i++); out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F)); break; case 14: char2 = str.charCodeAt(i++); char3 = str.charCodeAt(i++); out += String.fromCharCode(((c & 0x0F) << 12) | ((char2 & 0x3F) << 6) | ((char3 & 0x3F) << 0)); break } } return out }
     if (player_data.encrypt == '1') {
       player_data.url = unescape(player_data.url);
@@ -135,7 +146,7 @@ export default class extends Extension {
       player_data.url = unescape(base64decode(player_data.url));
       player_data.url_next = unescape(base64decode(player_data.url_next));
     }
-    const embed_url = `https://p2.cfnode1.xyz/ty4.php?url=${player_data.url}&url_next=${player_data.url_next}&id=${player_data.id}&nid=${player_data.nid}`;
+    const embed_url = `${embed_url_based}${player_data.url}&url_next=${player_data.url_next}&id=${player_data.id}&nid=${player_data.nid}`;
     const embedRes = await this.request("",{
       headers:{
         "Miru-Url":embed_url,
@@ -146,8 +157,11 @@ export default class extends Extension {
     const videoLink = embedRes.match(/urls = '(.+?)'/)[1];
     console.log(videoLink)
     return {
-      type: "mp4",
+      type: "hls",
       url: videoLink,//auto
+      headers:{
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+      }
 
     }
   }

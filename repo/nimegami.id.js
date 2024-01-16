1d2861083b87250137a481a932b42b89530ba4c9
Eugene van Aubel <35023@ma-web.nl>
2024-01-16 20:08:28 +0100

    Added change resolution on Nimegami

--- repo/nimegami.id.js
+++ repo/nimegami.id.js
@@ -10,6 +10,8 @@
 // @webSite      https://nimegami.id
 // ==/MiruExtension==
 
+//Current issue: Doesn't work on older animes such as spy x family since it's using video.nimegami.id instead of berkasdrive.com
+
 export default class extends Extension {
 
   async latest() {
@@ -67,38 +69,63 @@ export default class extends Extension {
     const title = await this.querySelector(res, 'h1.title').text
     const cover = await this.querySelector(res, 'img[itemprop="image"]').getAttributeText("src");
     const desc = "No Desc Avaliable";
-    const episodes = [];
+    const episodes_360p = [];
+    const episodes_480p = [];
+    const episodes_720p = [];
     const epiList = await this.querySelectorAll(res, "div.list_eps_stream > li");
 
     for (const element of epiList) {
       const html = await element.content;
       const name = await this.querySelector(html, ".select-eps").text;
-      const url = await this.getAttributeText(html, ".select-eps", "data");
+      const ep_url = await this.getAttributeText(html, ".select-eps", "data");
+      const words = CryptoJS.enc.Base64.parse(ep_url);
+      const textString = CryptoJS.enc.Utf8.stringify(words);
+      const regex_360 = /"format":"360p","url":\["([^"]+)","([^"]+)"]/;
+      const regex_480 = /"format":"480p","url":\["([^"]+)","([^"]+)"]/;
+      const regex_720 = /"format":"720p","url":\["([^"]+)","([^"]+)"]/;
+      const match_360 = textString.match(regex_360);
+      const match_480 = textString.match(regex_480);
+      const match_720 = textString.match(regex_720);
+      const url = match_360 ? match_360[2] : "";
+      const url_480 = match_480 ? match_480[2] : "";
+      const url_720 = match_720 ? match_720[2] : "";
 
-      episodes.push({
+
+      episodes_360p.push({
         name: name.trim(),
         url,
       });
+      episodes_480p.push({
+        name: name.trim(),
+        url: url_480,
+      });
+      episodes_720p.push({
+        name: name.trim(),
+        url: url_720,
+      });
     }
-    
     return{
       title: title.trim(),
       cover: cover,
       desc: desc,
       episodes:[{
-        title: 'Directory',
-        urls: episodes.reverse()
+        title: '360p',
+        urls: episodes_360p.reverse()
+      }, 
+      {
+        title: '480p',
+        urls: episodes_480p.reverse()
+      }, 
+      {
+        title: '720p',
+        urls: episodes_720p.reverse()
       }]
     }
   }
 
   async watch(url) {
-    const words = CryptoJS.enc.Base64.parse(url);
-    const textString = CryptoJS.enc.Utf8.stringify(words);
-    const regex = /"format":"720p","url":\["([^"]+)","([^"]+)"]/;
-    const match = textString.match(regex);
-    const episode_url = match ? match[2] : "";
-    const chec = episode_url.replace(/\\\//g, "/");
+    console.log(url)
+    const chec = url.replace(/\\\//g, "/");
     const res = await this.request('', {
       headers: {
         "Miru-Url": chec,

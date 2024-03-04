8040d1d4acb57f24ade077709a82d0eadb378291
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2024-03-05 02:36:28 +0530

    Update

--- repo/gogo.anime.js
+++ repo/gogo.anime.js
@@ -1,13 +1,13 @@
 // ==MiruExtension==
 // @name         GoGoAnime
-// @version      v0.0.4
+// @version      v0.0.5
 // @author       OshekharO
 // @lang         en
 // @license      MIT
 // @icon         https://play-lh.googleusercontent.com/MaGEiAEhNHAJXcXKzqTNgxqRmhuKB1rCUgb15UrN_mWUNRnLpO5T1qja64oRasO7mn0
 // @package      gogo.anime
 // @type         bangumi
-// @webSite      https://api-consumet-6adls650k-saurabhgangule.vercel.app/anime/gogoanime
+// @webSite      https://goraku-api.vercel.app/anime/gogoanime
 // ==/MiruExtension==
 
 export default class extends Extension {
@@ -25,7 +25,7 @@ export default class extends Extension {
       key: "gogoApi",
       type: "input",
       description: "API URL",
-      defaultValue: "https://api-consumet-6adls650k-saurabhgangule.vercel.app/anime/gogoanime",
+      defaultValue: "https://goraku-api.vercel.app/anime/gogoanime",
     });
     this.registerSetting({
       title: "Preferred quality",

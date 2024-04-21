029069825ec04a65c141bc583fef78fa9e7a550f
jarkad <109739558+jarkad@users.noreply.github.com>
2024-04-22 00:32:13 +0200

    Fix RoyalRoad

--- repo/royalroad.com.js
+++ repo/royalroad.com.js
@@ -1,6 +1,6 @@
 // ==MiruExtension==
 // @name         RoyalRoad
-// @version      v0.0.2
+// @version      v0.0.3
 // @author       appdevelpo
 // @lang         en
 // @license      MIT
@@ -11,82 +11,57 @@
 // @nsfw         false
 // ==/MiruExtension==
 
-export default class Biquge extends Extension {
+export default class extends Extension {
+    async fetchList(url) {
+        const res = await this.request(url)
+        const list = await this.querySelectorAll(res, '.fiction-list-item')
+        return await Promise.all(
+            list.map(async el => {
+                const html = await el.content
+                const title = await this.querySelector(html, '.fiction-title a').text
+                const url = await this.getAttributeText(html, '.fiction-title a', 'href')
+                const cover = await this.getAttributeText(html, 'img', 'src')
+                return { title, url, cover }
+            }),
+        )
+    }
+
     async latest(page) {
-        const res = await this.request(`/fictions/latest-updates?page=${page}`)
-        const liList = res.match(/<div class="fiction-list-item row">[\S\s]+?<\/div>/g)
-        const manga = []
-        liList.forEach(element => {
-            const url = element.match(/href="(.+?)"/)[1]
-            const title = element.match(/alt="(.+?)"/)[1]
-            const cover = element.match(/src="(.+?)"/)[1]
-            manga.push({
-                title,
-                url,
-                cover
-            })
-        });
-        return manga
+        return await this.fetchList(`/fictions/latest-updates?page=${page}`)
     }
 
     async search(kw, page) {
-        const res = await this.request(`/fictions/search?page=${page}&title=${kw}`)
-        const liList = res.match(/<div class="row fiction-list-item">[\S\s]+?<\/div>/g)
-        const manga = []
-       // console.log(res);
-        liList.forEach(element => {
-            const url = element.match(/href="(.+?)"/)[1]
-            const title = element.match(/alt="(.+?)"/)[1]
-            const cover = element.match(/src="(.+?)"/)[1]
-            manga.push({
-                title,
-                url,
-                cover
-            })
-        });
-        return manga
+        return await this.fetchList(`/fictions/search?page=${page}&title=${kw}`)
     }
 
     async detail(url) {
         const res = await this.request(url)
-        const content = res.match(/<script type="application\/ld\+json">[\s\S]+?<\/script>/)[0];
-        const title = res.match(/<title>(.+?)<\/title>/)[1];
-        const cover = content.match(/"image":"(.+?)"/)[1];
-        const desc = res.match(/"description":"(.+?)"/)[1];
-        const chapter = JSON.parse(res.match(/window.chapters = (\[[\s\S]+?\])/)[1]);
+        const title = await this.querySelector(res, 'h1').text
+        const cover = await this.getAttributeText(res, '.cover-image-container img', 'src')
+        const desc = await this.querySelector(res, '.description .hidden-content').text.then(t => t.trim())
+        const chapters = await this.querySelectorAll(res, '#chapters .chapter-row').then(chaps =>
+            Promise.all(
+                chaps.map(async el => {
+                    const html = await el.content
+                    const name = await this.querySelector(html, 'a').text.then(t => t.trim())
+                    const url = await this.getAttributeText(html, 'a', 'href')
+                    return { name, url }
+                }),
+            ),
+        )
         return {
             title,
             cover,
             desc,
-            episodes: [{
-                title: "Directory",
-                urls: chapter.map((item) => ({
-                    name: item.title,
-                    url: item.url
-                }))
-            }]
+            episodes: [{ title: 'Chapters', urls: chapters }],
         }
-
     }
 
     async watch(url) {
-    const res = await this.request(url);
-    const content = res.match(/<p>[^&]+?<\/p>/g);
-    const new_content = [];
-
-    if (content) {
-        content.forEach((e) => {
-            e = e.replace(/<.+?>/g, "");
-            console.log(e);
-            new_content.push(e);
-        });
+        const res = await this.request(url)
+        const title = await this.querySelector(res, 'h1').text
+        const text = await this.querySelector(res, '.chapter-content').text
+        const content = text.split('<br>').map(t => t.trim())
+        return { title, content }
     }
-
-    const title = res.match(/>(.+?)<\/h1>/)[0];
-    return {
-        content: new_content,
-        title,
-    };
-}
-
 }

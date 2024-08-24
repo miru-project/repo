de2b19866a90bd33b230b775033ba406f4619049
Hualiang <78242797+hualiong@users.noreply.github.com>
2024-08-24 16:56:22 +0800

    Update cycanime.com.js

--- repo/cycanime.com.js
+++ repo/cycanime.com.js
@@ -1,6 +1,6 @@
 // ==MiruExtension==
 // @name         次元城动漫
-// @version      v1.0.0
+// @version      v1.0.1
 // @author       hualiang
 // @lang         zh
 // @license      MIT
@@ -12,45 +12,13 @@
 // ==/MiruExtension==
 export default class extends Extension {
   async load() {
-    this.base64decode = (str) => {
-      var base64DecodeChars = [
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59,
-        60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
-        43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1,
-      ];
-      var c1, c2, c3, c4;
-      var i, len, out;
-      len = str.length;
-      i = 0;
-      out = "";
-      while (i < len) {
-        do {
-          c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
-        } while (i < len && c1 == -1);
-        if (c1 == -1) break;
-        do {
-          c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
-        } while (i < len && c2 == -1);
-        if (c2 == -1) break;
-        out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
-        do {
-          c3 = str.charCodeAt(i++) & 0xff;
-          if (c3 == 61) return out;
-          c3 = base64DecodeChars[c3];
-        } while (i < len && c3 == -1);
-        if (c3 == -1) break;
-        out += String.fromCharCode(((c2 & 0xf) << 4) | ((c3 & 0x3c) >> 2));
-        do {
-          c4 = str.charCodeAt(i++) & 0xff;
-          if (c4 == 61) return out;
-          c4 = base64DecodeChars[c4];
-        } while (i < len && c4 == -1);
-        if (c4 == -1) break;
-        out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+    this.getEp = (url, sources) => {
+      const n = url.split("/");
+      let ep = parseInt(n[4].split(".")[0]);
+      for (let i = 0; i < n[3] - 1; i++) {
+        ep += sources[i].total;
       }
-      return out;
+      return ep;
     };
     this.querySelector = async (content, selector) => {
       const res = await this.querySelectorAll(content, selector);
@@ -113,35 +81,40 @@ export default class extends Extension {
       metadata.set(key, value);
     }
     const desc = this.text(await this.querySelector(res, "#height_limit")).replace("&nbsp;", "");
-    const sources = (await this.querySelectorAll(res, ".anthology-tab a")).map((e) =>
-      e.content.match(/i>(.*?)</)[1].replace("&nbsp;", "")
-    );
+    const sources = (await this.querySelectorAll(res, ".anthology-tab a")).map((e) => {
+      const ep = e.content.match(/<span class="badge">(.*?)</);
+      return {
+        name: e.content.match(/i>(.*?)</)[1].replace("&nbsp;", ""),
+        total: !ep ? 1 : parseInt(ep[1]),
+      };
+    });
     const videos = await this.querySelectorAll(res, ".anthology-list-play");
     const episodes = [];
     for (let i = 0; i < videos.length; i++) {
       const urls = await this.querySelectorAll(videos[i].content, "a");
       for (let j = 0; j < urls.length; j++) {
-        urls[j] = { name: this.text(urls[j]), url: `${await urls[j].getAttributeText("href")}|${data[1]}` };
+        urls[j] = {
+          name: this.text(urls[j]),
+          url: `${this.getEp(await urls[j].getAttributeText("href"), sources)}|${data[1]}`,
+        };
       }
-      episodes.push({ title: sources[i], urls });
+      episodes.push({ title: sources[i].name, urls });
     }
     return { title: data[1], cover: data[2], metadata: Object.fromEntries(metadata), desc, episodes };
   }
 
   async watch(str) {
     const d = str.split("|");
-    const ep = d[0].split("/")[4];
-    const res = await this.request(`/api/anime?msg=${d[1]}&n=1&type=1&j=${ep.split(".")[0]}`, {
+    const res = await this.request(`/api/anime?msg=${d[1]}&n=1&type=1&j=${d[0]}`, {
       headers: { "Miru-Url": "https://oiapi.net" },
     });
     const { data } = JSON.parse(JSON.stringify(res));
     return { type: data.play_url.indexOf(".mp4") > 0 ? "mp4" : "hls", url: data.play_url };
   }
-
-  // async watch(url) {
-  //   const res = await this.request(url);
-  //   const json = JSON.parse(this.text(await this.querySelector(res, ".player-left > script:nth-child(1)")).substring(16));
-  //   const link = json.encrypt == 1 ? decodeURIComponent(json.url) : decodeURIComponent(this.base64decode(json.url));
-  //   return { type: link.indexOf(".mp4") > 0 ? "mp4" : "hls", url: link };
-  // }
+  
+  async checkUpdate(str) {
+    const url = str.split("|")[0];
+    const res = await this.request(url);
+    return this.text(await this.querySelector(res, ".slide-info > .slide-info-remarks:nth-child(1)"));
+  }
 }

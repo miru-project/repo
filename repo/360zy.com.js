b44a0e6d1db4cfef966e3a2ce668b10fbaee36a1
周华梁 <642615676@qq.com>
2024-09-17 03:04:29 +0800

    Add some video resource collection stations

---
+++ repo/360zy.com.js
@@ -0,0 +1,145 @@
+// ==MiruExtension==
+// @name         360资源
+// @version      v0.0.1
+// @author       hualiong
+// @lang         zh-cn
+// @license      MIT
+// @icon         https://360zy.com/favicon.ico
+// @package      360zy.com
+// @type         bangumi
+// @webSite      https://360zy.com
+// @nsfw         false
+// ==/MiruExtension==
+export default class extends Extension {
+  genres = {};
+
+  domains = {
+    primary: [
+      "360zy.com",
+      "360zy.net",
+      "360zy.top",
+      "360zy.tv",
+    ],
+    alternate: [
+      "360zy1.com",
+      "360zy2.com",
+      "360zy3.com",
+      "360zy4.com",
+      "360zy5.com",
+      "360zy6.com",
+      "360zy7.com",
+      "360zy8.com",
+      "360zy9.com",
+      "360zy10.com",
+    ],
+  };
+
+  dict = new Map([
+    ["&nbsp;", " "],
+    ["&quot;", '"'],
+    ["&lt;", "<"],
+    ["&gt;", ">"],
+    ["&amp;", "&"],
+    ["&sdot;", "·"],
+  ]);
+
+  text(content) {
+    if (!content) return "";
+    const str =
+      [...content.matchAll(/>([^<]+?)</g)]
+        .map((m) => m[1])
+        .join("")
+        .trim() || content;
+    return str.replace(/&[a-z]+;/g, (c) => this.dict.get(c) || c);
+  }
+
+  async $get(params, count = 2, timeout = 4000) {
+    const domains = count > 1 ? this.domains.primary : this.domains.alternate;
+    try {
+      const list = domains.map((domain) =>
+        this.request(`/api.php/provide/vod?ac=detail${params}`, {
+          headers: { "Miru-Url": `https://${domain}` },
+        })
+      );
+      list.push(
+        new Promise((_, reject) => {
+          setTimeout(() => {
+            reject(new Error("Request timed out!"));
+          }, timeout);
+        })
+      );
+      return await Promise.any(list);
+    } catch (error) {
+      if (count > 1) {
+        console.log(`[Retry (${count})]: ${params}`);
+        return this.$get(params, count - 1);
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async load() {
+    const res = await this.$get("&ac=list");
+    res.class.forEach((e) => {
+      this.genres[e.type_id] = e.type_name;
+    });
+  }
+
+  async createFilter() {
+    const genres = {
+      title: "影片类型",
+      max: 1,
+      min: 0,
+      default: "",
+      options: this.genres,
+    };
+    return { genres };
+  }
+
+  async latest(page) {
+    const h = (new Date().getUTCHours() + 9) % 24;
+    const res = await this.$get(`&pg=${page}&h=${h || 24}`);
+    return res.list.map((e) => ({
+      title: e.vod_name,
+      url: `${e.vod_id}`,
+      cover: e.vod_pic,
+      update: e.vod_remarks,
+    }));
+  }
+
+  async search(kw, page, filter) {
+    if (!kw && !(filter.genres && filter.genres[0])) {
+      return this.latest(page);
+    }
+    const res = await this.$get(`&wd=${kw}&t=${filter.genres[0] ?? ""}&pg=${page}`);
+    return res.list.map((e) => ({
+      title: e.vod_name,
+      url: `${e.vod_id}`,
+      cover: e.vod_pic,
+      update: e.vod_remarks,
+    }));
+  }
+
+  async detail(id) {
+    let desc = "无";
+    const anime = (await this.$get(`&ids=${id}`)).list[0];
+    const blurb = this.text(anime.vod_blurb);
+    const content = this.text(anime.vod_content);
+    desc = desc.length < blurb?.length ? blurb : desc;
+    desc = desc.length < content.length ? content : desc;
+    const urls = anime.vod_play_url
+      .split("#")
+      .filter((e) => e)
+      .map((e) => {
+        const s = e.split("$");
+        return { name: s[0], url: s[1] };
+      });
+    return { title: anime.vod_name, cover: anime.vod_pic, desc, episodes: [{ title: this.name, urls }] };
+  }
+
+  async watch(url) {
+    console.log(url);
+    return { type: "hls", url };
+  }
+}
---
+++ repo/moduzy.com.js
@@ -0,0 +1,136 @@
+// ==MiruExtension==
+// @name         魔都资源网
+// @version      v0.0.1
+// @author       hualiong
+// @lang         zh-cn
+// @license      MIT
+// @icon         https://moduzy.vip/favicon.ico
+// @package      moduzy.com
+// @type         bangumi
+// @webSite      https://moduzy.vip
+// @nsfw         true
+// ==/MiruExtension==
+export default class extends Extension {
+  genres = {};
+
+  domains = [
+    "moduzy1.com",
+    "moduzy2.com",
+    "moduzy3.com",
+    "moduzy4.com",
+    "moduzy5.com",
+    "moduzy6.com",
+    "moduzy7.com",
+    "moduzy8.com",
+    "moduzy9.com",
+    "moduzy10.com",
+  ];
+
+  dict = new Map([
+    ["&nbsp;", " "],
+    ["&quot;", '"'],
+    ["&lt;", "<"],
+    ["&gt;", ">"],
+    ["&amp;", "&"],
+    ["&sdot;", "·"],
+  ]);
+
+  text(content) {
+    if (!content) return "";
+    const str =
+      [...content.matchAll(/>([^<]+?)</g)]
+        .map((m) => m[1])
+        .join("")
+        .trim() || content;
+    return str.replace(/&[a-z]+;/g, (c) => this.dict.get(c) || c);
+  }
+
+  async $get(params, count = 2, timeout = 4000) {
+    try {
+      const list = this.domains.map((e) =>
+        this.request(`/api.php/provide/vod?ac=detail${params}`, {
+          headers: { "Miru-Url": `https://${e}` },
+        })
+      );
+      list.push(
+        new Promise((_, reject) => {
+          setTimeout(() => {
+            reject(new Error("Request timed out!"));
+          }, timeout);
+        })
+      );
+      return await Promise.any(list);
+    } catch (error) {
+      if (count > 1) {
+        console.log(`[Retry (${count})]: ${params}`);
+        return this.$get(params, count - 1);
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async load() {
+    const res = await this.$get("&ac=list");
+    res.class.forEach((e) => {
+      this.genres[e.type_id] = e.type_name;
+    });
+  }
+
+  async createFilter() {
+    const genres = {
+      title: "影片类型",
+      max: 1,
+      min: 0,
+      default: "",
+      options: this.genres,
+    };
+    return { genres };
+  }
+
+  async latest(page) {
+    const h = (new Date().getUTCHours() + 9) % 24;
+    const res = await this.$get(`&pg=${page}&h=${h || 24}`);
+    return res.list.map((e) => ({
+      title: e.vod_name,
+      url: `${e.vod_id}`,
+      cover: e.vod_pic,
+      update: e.vod_remarks,
+    }));
+  }
+
+  async search(kw, page, filter) {
+    if (!kw && !(filter.genres && filter.genres[0])) {
+      return this.latest(page);
+    }
+    const res = await this.$get(`&wd=${kw}&t=${filter.genres[0] ?? ""}&pg=${page}`);
+    return res.list.map((e) => ({
+      title: e.vod_name,
+      url: `${e.vod_id}`,
+      cover: e.vod_pic,
+      update: e.vod_remarks,
+    }));
+  }
+
+  async detail(id) {
+    let desc = "无";
+    const anime = (await this.$get(`&ids=${id}`)).list[0];
+    const blurb = this.text(anime.vod_blurb);
+    const content = this.text(anime.vod_content);
+    desc = desc.length < blurb?.length ? blurb : desc;
+    desc = desc.length < content.length ? content : desc;
+    const urls = anime.vod_play_url
+      .split("#")
+      .filter((e) => e)
+      .map((e) => {
+        const s = e.split("$");
+        return { name: s[0], url: s[1] };
+      });
+    return { title: anime.vod_name, cover: anime.vod_pic, desc, episodes: [{ title: this.name, urls }] };
+  }
+
+  async watch(url) {
+    console.log(url);
+    return { type: "hls", url };
+  }
+}

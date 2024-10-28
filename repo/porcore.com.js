9606fac8a842aed6b8f94efed4a062fb8bb8c51d
ijs77 <121365263+ijs77@users.noreply.github.com>
2024-10-28 17:39:14 +0700

    Update porcore.com.js
    add filter

--- repo/porcore.com.js
+++ repo/porcore.com.js
@@ -1,6 +1,6 @@
 // ==MiruExtension==
 // @name         PorCore
-// @version      v0.0.1
+// @version      v0.0.2
 // @author       javxsub.com
 // @lang         en
 // @license      MIT
@@ -13,97 +13,121 @@
 
 export default class extends Extension {
 
-  async req(url) {
-    const res = await this.request("", {
-      "Miru-Url": url,
-    });
-    return url;
-  }
-
-  async latest(page) {
-    // Latest updates
-    const url = `/?ajax&p=${page}`;
-    const res = await this.request(url);
-    const videoList = await this.querySelectorAll(res, "div.onevideothumb");
-    const videos = [];
-
-    for (const element of videoList) {
-        const html  = await element.content;
-        const title = await this.getAttributeText(html, "h5", "title");
-        const url   = await this.getAttributeText(html, "a", "href");
-        const cover = await this.getAttributeText(html, "img.flipbookimages", "src");
-        const updt  = await this.querySelector(html, "div.floatlefttop").text;
-    
-        if (title && url && cover && cover.includes("res.php")) {
-            videos.push({
-                title: title,
-                url: url,
-                cover: cover,
-                update: updt.trim(),
-            });
-        }
+    async req(url) {
+        const res = await this.request("", {
+            "Miru-Url": url,
+        });
+        return url;
     }
 
-    return videos;
-  }
-
-  async search(kw, page) {
-    // Search
-    const url = `/show/${kw}?ajax&sort=newest&p=${page}`;
-    const res = await this.request(url);
-    const videoList = await this.querySelectorAll(res, "div.onevideothumb");
-    const videos = [];
-
-    for (const element of videoList) {
-        const html  = await element.content;
-        const title = await this.getAttributeText(html, "h5", "title");
-        const url   = await this.getAttributeText(html, "a", "href");
-        const cover = await this.getAttributeText(html, "img.flipbookimages", "src");
-        const updt  = await this.querySelector(html, "div.floatlefttop").text;
-    
-        if (title && url && cover && cover.includes("res.php")) {
-            videos.push({
-                title: title,
-                url: url,
-                cover: cover,
-                update: updt.trim(),
-            });
+    async createFilter(filter) {
+        const filters = {
+            sort_by: {
+                title: "Sort By",
+                max: 1,
+                min: 1,
+                default: "",
+                options: {
+                    "&sort=newest": "Newest",
+                    "&sort=oldest": "Oldest",
+                    "&sort=longest": "Longest",
+                    "&sort=shortest": "Shortest",
+                    "&sort=mostviewed": "Most Viewed",
+                    "&sort=toprated": "Top Rated",
+                    "&sort=relevancy": "Relevancy",
+                    "": "None"
+                }
+            }
         }
+        return filters
     }
 
-    return videos;
-  }
+    async latest(page) {
+        // Latest updates
+        const url = `/?ajax&p=${page}`;
+        const res = await this.request(url);
+        const videoList = await this.querySelectorAll(res, "div.onevideothumb");
+        const videos = [];
 
-  async detail(url) {
-    // Details
-    const strippedpath = url.replace(/^(https?:\/\/)?([^\/]+)(\/.*)?/, '$3');
-    const res   = await this.request(strippedpath);
-    const title = await this.querySelector(res, 'h1').text;
-    const cover = await this.querySelector(res, 'video.video-js').getAttributeText("poster");
-    const desc  = await this.querySelector(res, 'p.small').text;
-    const user  = await this.querySelector(res, 'a.label-default').text;
-    const video = await this.querySelector(res, 'source[type="application\/x-mpegURL"]').getAttributeText("src");
+        for (const element of videoList) {
+            const html = await element.content;
+            const title = await this.getAttributeText(html, "h5", "title");
+            const url = await this.getAttributeText(html, "a", "href");
+            const cover = await this.getAttributeText(html, "img.flipbookimages", "src");
+            const updt = await this.querySelector(html, "div.floatlefttop").text;
 
-    return {
-      title: title.trim(),
-      cover: cover,
-      desc,
-      episodes: [
-        {
-          title: user.trim(),
-          urls: [{
-            name: title.trim(),
-            url: video,
-          }]
-        },
-      ],
-    };
-  }
+            if (title && url && cover && cover.includes("res.php")) {
+                videos.push({
+                    title: title,
+                    url: url,
+                    cover: cover,
+                    update: updt.trim(),
+                });
+            }
+        }
 
-  async watch(url) {
-    return {
-      type: "hls",
-      url: url || "",
-    };
-  }
+        return videos;
+    }
+
+    async search(kw, page, filter) {
+        // Search
+        if (kw) {
+            var search_string = `/show/${kw}?p=${page}`;
+        } else {
+            var search_string = `/?ajax&p=${page}`;
+        }
+        search_string += filter['sort_by']
+        const res = await this.request(search_string);
+        const videoList = await this.querySelectorAll(res, "div.onevideothumb");
+        const videos = [];
+
+        for (const element of videoList) {
+            const html = await element.content;
+            const title = await this.getAttributeText(html, "h5", "title");
+            const url = await this.getAttributeText(html, "a", "href");
+            const cover = await this.getAttributeText(html, "img.flipbookimages", "src");
+            const updt = await this.querySelector(html, "div.floatlefttop").text;
+
+            if (title && url && cover && cover.includes("res.php")) {
+                videos.push({
+                    title: title,
+                    url: url,
+                    cover: cover,
+                    update: updt.trim(),
+                });
+            }
+        }
+
+        return videos;
+    }
+
+    async detail(url) {
+        // Details
+        const strippedpath = url.replace(/^(https?:\/\/)?([^\/]+)(\/.*)?/, '$3');
+        const res = await this.request(strippedpath);
+        const title = await this.querySelector(res, 'h1').text;
+        const cover = await this.querySelector(res, 'video.video-js').getAttributeText("poster");
+        const desc = await this.querySelector(res, 'p.small').text;
+        const user = await this.querySelector(res, 'a.label-default').text;
+        const video = await this.querySelector(res, 'source[type="application\/x-mpegURL"]').getAttributeText("src");
+        return {
+            title: title.trim(),
+            cover: cover,
+            desc,
+            episodes: [{
+                title: user.trim(),
+                urls: [{
+                    name: title.trim(),
+                    url: video,
+                }]
+            }, ],
+        };
+    }
+
+    async watch(url) {
+        return {
+            type: "hls",
+            url: url || "",
+        };
+    }
 }

35d8fe674d1a1829d221a36213cdf2fd81b87101
Virus <smiledie-hub@yandex.ru>
2025-08-15 18:35:25 +0500

    fix aniliberty repo

--- repo/aniliberty.js
+++ repo/aniliberty.js
@@ -1,52 +1,83 @@
 // ==MiruExtension==
 // @name         AniLiberty
-// @version      v0.0.4
+// @version      v0.0.5
 // @author       Virus (viridius-hub)
 // @lang         ru
 // @license      MIT
 // @icon         https://anilibria.top/static/favicon-96x96.png
 // @package      aniliberty
 // @type         bangumi
-// @webSite      https://aniliberty.top/api/v1
+// @webSite      https://anilibria.top/api/v1
 // @nsfw         false
 // ==/MiruExtension==
 
 export default class extends Extension {
-    async load() {
-        this.registerSetting({
-            title: "AniLiberty",
-            key: "domain_aniliberty",
-            type: "input",
-            description: "AniLiberty Domain",
-            defaultValue: "https://aniliberty.top",
-        });
-    }
+  async load() {
+    this.registerSetting({
+      title: "AniLiberty",
+      key: "domain_aniliberty",
+      type: "input",
+      description: "AniLiberty Domain",
+      defaultValue: "https://anilibria.top",
+    });
+  }
 
-    async req(url) {
-        return this.request(url, {
-            headers: {
-                "Miru-Url": await this.getSetting("domain_aniliberty"),
-            },
-        });
-    }
+  async req(url) {
+    return this.request(url, {
+      headers: {
+        "Miru-Url": await this.getSetting("domain_aniliberty"),
+      },
+    });
+  }
 
-    async latest(page) {
-        console.log(url);
-        return []
-    }
+  async latest(page) {
+    const res = await this.req(`/api/v1/anime/releases/latest?limit=42`);
 
-    async detail(url) {
-        console.log(url);
-        return {}
-    }
+    console.log(res)
 
-    async search(kw, page) {
-        console.log(url);
-        return []
-    }
+    return res.results.map((item) => ({
+      url: `/api/v1/anime/releases/${item.alias}`,
+      title: item.name.main,
+      cover: item.poster.src,
+    }));
+  }
 
-    async watch(url) {
-        console.log(url);
-        return {}
-    }
+  async detail(url) {
+    const res = await this.req(`${url}`);
+    return {
+      title: res.name.main,
+      cover: res.poster.src,
+      desc: res.description,
+      episodes: [
+        {
+          title: "Ep",
+          urls: res.episodes.map((item) => ({
+            name: `${item.ordinal}`,
+            url: item.hls_1080,
+          })),
+        },
+      ],
+    };
+  }
+
+  async search(kw, page) {
+    const res = await this.req(`/api/v1/app/search/releases?query=${kw}`);
+    return res.results.map((item) => ({
+      title: item.name.main,
+      url: `/api/v1/anime/releases/${item.alias}`,
+      cover: item.poster.src,
+      desc: item.description,
+    }));
+  }
+
+  async watch(url) {
+    const res = await this.req(`${url}`);
+    const srv = Object.values(res.servers).filter(
+        (server) => server.name === "Our Server"
+    );
+    return {
+      type: "hls",
+      url: srv[0].file_url,
+    };
+  }
 }

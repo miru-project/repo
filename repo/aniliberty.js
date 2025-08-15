7f825a41616221ef96a12fccc8cb74aeba0f4498
Virus <smiledie-hub@yandex.ru>
2025-08-15 18:30:44 +0500

    fix aniliberty repo

--- repo/aniliberty.js
+++ repo/aniliberty.js
@@ -1,34 +1,52 @@
 // ==MiruExtension==
 // @name         AniLiberty
-// @version      v0.0.3
+// @version      v0.0.4
 // @author       Virus (viridius-hub)
 // @lang         ru
 // @license      MIT
 // @icon         https://anilibria.top/static/favicon-96x96.png
-// @package      aniliberty.top
+// @package      aniliberty
 // @type         bangumi
-// @webSite      https://anilibria.top/api/v1
+// @webSite      https://aniliberty.top/api/v1
 // @nsfw         false
 // ==/MiruExtension==
 
 export default class extends Extension {
-  async latest(page) {
-    console.log(url);
-    return []
-  }
+    async load() {
+        this.registerSetting({
+            title: "AniLiberty",
+            key: "domain_aniliberty",
+            type: "input",
+            description: "AniLiberty Domain",
+            defaultValue: "https://aniliberty.top",
+        });
+    }
 
-  async detail(url) {
-    console.log(url);
-    return {}
-  }
+    async req(url) {
+        return this.request(url, {
+            headers: {
+                "Miru-Url": await this.getSetting("domain_aniliberty"),
+            },
+        });
+    }
 
-  async search(kw, page) {
-    console.log(url);
-    return []
-  }
+    async latest(page) {
+        console.log(url);
+        return []
+    }
 
-  async watch(url) {
-    console.log(url);
-    return {}
-  }
+    async detail(url) {
+        console.log(url);
+        return {}
+    }
+
+    async search(kw, page) {
+        console.log(url);
+        return []
+    }
+
+    async watch(url) {
+        console.log(url);
+        return {}
+    }
 }

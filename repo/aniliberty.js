8d991ae2c902916a9db682bb6d20737146944409
Virus <smiledie-hub@yandex.ru>
2025-08-15 18:20:20 +0500

    fix aniliberty repo

--- repo/aniliberty.js
+++ repo/aniliberty.js
@@ -12,65 +12,23 @@
 // ==/MiruExtension==
 
 export default class extends Extension {
-  async req(url) {
-    return this.request(url, {
-      headers: {
-        "Miru-Url": "https://anilibria.top/api/v1",
-      },
-    });
-  }
-
-  async load() {
-
-  }
-
   async latest(page) {
-    const res = await this.req(
-        `/anime/releases/latest?limit=42`
-    );
-    return res.results.map((item) => ({
-      url: `/anime/releases/${item.alias}`,
-      title: item.name.main,
-      cover: item.poster.src,
-    }));
+    console.log(url);
+    return []
   }
 
   async detail(url) {
-    const res = await this.req(`${url}`);
-    return {
-      title: res.name.main,
-      cover: res.poster.src,
-      desc: res.description,
-      episodes: [
-        {
-          title: "Episodes",
-          urls: res.episodes.map((item) => ({
-            name: `${item.ordinal}`,
-            url: item.hls_1080,
-          })),
-        },
-      ],
-    };
+    console.log(url);
+    return {}
   }
 
   async search(kw, page) {
-    const res = await this.req(`/app/search/releases?query=${kw}`);
-    return res.results.map((item) => ({
-      title: item.name.main,
-      url: `/anime/releases/${item.alias}`,
-      cover: item.poster.src,
-      desc: item.description,
-    }));
+    console.log(url);
+    return []
   }
 
   async watch(url) {
-    const res = await this.req(`${url}`);
-    const srv = Object.values(res.servers).filter(
-      (server) => server.name === "Our Server"
-    );
-    return {
-      type: "hls",
-      url: srv[0].file_url,
-    };
+    console.log(url);
+    return {}
   }
 }

8e18a566315aacb02e5429a4ed5a748921f9e0e2
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2026-09-21 22:05:55 +0530

    Fix Ravenscans extension domain and parser (#16)

--- README.md
+++ README.md
@@ -80,7 +80,7 @@ Miru extensions repository | [Miru App Download](https://github.com/miru-project
 | NyaFun动漫 | nyadm.link | v0.0.6 | hualiong | zh-cn | bangumi | [Source Code](https://github.com/miru-project/repo/blob/main/repo/nyadm.link.js) |
 | Otakudesu | otakudesu | v0.0.1 | Nazz | id | bangumi | [Source Code](https://github.com/miru-project/repo/blob/main/repo/otakudesu.js) |
 | Piped | piped.video | v0.0.1 | bethro | all | bangumi | [Source Code](https://github.com/miru-project/repo/blob/main/repo/piped.video.js) |
-| Ravenscans | ravenscans.com | v0.0.1 | bethro | en | manga | [Source Code](https://github.com/miru-project/repo/blob/main/repo/ravenscans.com.js) |
+| Ravenscans | ravenscans.com | v0.0.2 | bethro | en | manga | [Source Code](https://github.com/miru-project/repo/blob/main/repo/ravenscans.com.js) |
 | rawkuma | rawkuma.com | v0.0.2 | appdevelpo | jp | manga | [Source Code](https://github.com/miru-project/repo/blob/main/repo/rawkuma.com.js) |
 | ReadComicsOnline | readcomicsonline.ru | v0.0.2 | OshekharO | en | manga | [Source Code](https://github.com/miru-project/repo/blob/main/repo/readcomicsonline.ru.js) |
 | ACG.RIP | rip.acg | v0.0.1 | MiaoMint | zh-cn | bangumi | [Source Code](https://github.com/miru-project/repo/blob/main/repo/rip.acg.js) |
--- repo/ravenscans.com.js
+++ repo/ravenscans.com.js
@@ -1,13 +1,13 @@
 // ==MiruExtension==
 // @name         Ravenscans
-// @version      v0.0.1
+// @version      v0.0.2
 // @author       bethro
 // @lang         en
 // @license      MIT
-// @icon         https://i0.wp.com/ravenscans.com/wp-content/uploads/2022/12/cropped-33-192x192.png
+// @icon         https://ravenscans.net/wp-content/uploads/2025/05/logo.png
 // @package      ravenscans.com
 // @type         manga
-// @webSite      https://ravenscans.com
+// @webSite      https://ravenscans.org
 // ==/MiruExtension==
 
 export default class extends Extension {
@@ -25,46 +25,60 @@ export default class extends Extension {
             key: "ravenscans",
             type: "input",
             description: "Homepage URL for Ravenscans",
-            defaultValue: "https://ravenscans.com",
+            defaultValue: "https://ravenscans.org",
         });
     }
 
     async latest(page) {
-        const res = await this.req(`/page/${page}/`);
-        const latest = await this.querySelectorAll(res, "div.utao.styletwo");
+        const res = await this.req(`/manga/?page=${page}&order=update`);
+        const bsxList = res.match(/<div class="bsx">([\s\S]+?)<\/div>\s*<\/div>/g) || [];
 
-        let comic = []
-        for (const element of latest) {
-            const html = await element.content;
-            const url = (await this.getAttributeText(html, "a", "href"));
-            const title = await this.querySelector(html, "h4").text;
-            const cover = await this.querySelector(html, "img").getAttributeText("src");
+        const comic = [];
+        for (const item of bsxList) {
+            const urlMatch = item.match(/href="([^"]+)"/);
+            const titleMatch = item.match(/<div class="tt">[\s\S]*?([^\s<][^<]*[^\s<])/) || item.match(/title="([^"]+)"/);
+            const coverMatch = item.match(/src="([^"]+)"/);
 
-            comic.push({
-                title: title.trim(),
-                url,
-                cover: cover
-            })
+            const url = urlMatch ? urlMatch[1] : "";
+            const title = titleMatch ? titleMatch[1].replace(/&#8217;/g, "'").trim() : "";
+            const cover = coverMatch ? coverMatch[1] : "";
+
+            if (url && title) {
+                comic.push({
+                    title,
+                    url,
+                    cover,
+                });
+            }
         }
         return comic;
     }
 
     async search(kw, page) {
         const res = await this.req(`/page/${page}/?s=${kw}`);
-        const searchList = await this.querySelectorAll(res, "div.bsx");
-        const result = await Promise.all(searchList.map(async (element) => {
-            const html = await element.content;
-            const url = await this.getAttributeText(html, "a", "href");
-            const title = await this.getAttributeText(html, "a", "title");
-            const cover = await this.querySelector(html, "img").getAttributeText("src");
-            return {
-                title: title.trim(),
-                url,
-                cover: cover
-            };
-        }));
+        const bsxList = res.match(/<div class="bsx">([\s\S]+?)<\/div>\s*<\/div>/g) || [];
+
+        const result = [];
+        for (const item of bsxList) {
+            const urlMatch = item.match(/href="([^"]+)"/);
+            const titleMatch = item.match(/<div class="tt">[\s\S]*?([^\s<][^<]*[^\s<])/) || item.match(/title="([^"]+)"/);
+            const coverMatch = item.match(/src="([^"]+)"/);
+
+            const url = urlMatch ? urlMatch[1] : "";
+            const title = titleMatch ? titleMatch[1].replace(/&#8217;/g, "'").trim() : "";
+            const cover = coverMatch ? coverMatch[1] : "";
+
+            if (url && title) {
+                result.push({
+                    title,
+                    url,
+                    cover,
+                });
+            }
+        }
         return result;
     }
+
     async detail(url) {
         const res = await this.request("", {
             headers: {
@@ -72,24 +86,30 @@ export default class extends Extension {
             },
         });
 
-        const title = await this.querySelector(res, "div.infox > h1").text;
-        const cover = await this.querySelector(res, "img.wp-post-image").getAttributeText("src");
-        const desc = await this.querySelector(res, "div.entry-content.entry-content-single > p").text;
-        const epiList = await this.querySelectorAll(res, "div.eplister > ul > li");
-        const episodes = await Promise.all(epiList.map(async (element) => {
-            const html = await element.content;
-            const name = await this.querySelector(html, "span.chapternum").text;
-            const url = await this.getAttributeText(html, "a", "href");
+        const titleMatch = res.match(/<h1[^>]*class="entry-title"[^>]*>([\s\S]+?)<\/h1>/i) || res.match(/class="infox"[\s\S]*?<h1[^>]*>([\s\S]+?)<\/h1>/i);
+        const title = titleMatch ? titleMatch[1].replace(/<[^>]+>/g, "").replace(/&#8217;/g, "'").trim() : "Unknown Title";
+
+        const coverMatch = res.match(/<img[^>]*class="[^"]*wp-post-image[^"]*"[^>]*src="([^"]+)"/i) || res.match(/<div class="thumb">[\s\S]*?<img[^>]*src="([^"]+)"/i);
+        const cover = coverMatch ? coverMatch[1] : "";
+
+        const descMatch = res.match(/<div[^>]*class="entry-content[^"]*"[^>]*>([\s\S]+?)<\/div>/i);
+        const desc = descMatch ? descMatch[1].replace(/<[^>]+>/g, "").trim() : "";
+
+        const chMatches = [...res.matchAll(/<li[^>]*data-num="([^"]*)"[^>]*>[\s\S]*?<a[^>]*href="([^"]+)"[^>]*>[\s\S]*?<span[^>]*class="chapternum"[^>]*>([\s\S]+?)<\/span>/gi)];
+
+        const episodes = chMatches.map((m) => {
+            const chUrl = m[2];
+            const chName = m[3].replace(/<[^>]+>/g, "").trim();
             return {
-                name,
-                url: url,
+                name: chName || `Chapter ${m[1]}`,
+                url: chUrl,
             };
-        }));
+        });
 
         return {
-            title: title || "Unknown Title",
+            title,
             cover,
-            desc: desc || "",
+            desc,
             episodes: [
                 {
                     title: "Chapters",
@@ -114,8 +134,9 @@ export default class extends Extension {
 
         const imagesContent = match[1];
 
-        const imageUrls = imagesContent.match(/"([^"]+)"/g).map(match => match.slice(1, -1).replace(/\\/g, ''));
+        const imageUrls = (imagesContent.match(/"([^"]+)"/g) || [])
+            .map(m => m.slice(1, -1).replace(/\\/g, ''));
 
         return { urls: imageUrls };
     }
-} 
+}
